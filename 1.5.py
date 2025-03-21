5d97008772c20c3ee686753288607a9e9e237e80
narasy30 <reddyakulavenkata@gmail.com>
2025-03-21 15:23:25 +0530

    Add files via upload

---
+++ 1.5.py
@@ -0,0 +1,4 @@
+base=float(input("base:"))
+height=float(input("height:"))
+area=0.3*base*height
+print("area of triangle:",area)
---
+++ 1.6.py
@@ -0,0 +1,6 @@
+length=float(input("enter the length of the rectangle:"))
+width=float(input("enter the width of the rectangle:"))
+area=length*width
+perimeter=2*(length+width)
+print("area of rectangle is",area)
+print("perimeter of rectangleis",perimeter)
