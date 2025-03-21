49e6ced8b9999be146fe458f490fae7d282d34ef
narasy30 <reddyakulavenkata@gmail.com>
2025-03-21 15:22:35 +0530

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,29 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 07:55:33) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter first number:"))
+enter first number:20
+>>> b=int(input("enter second number:"))
+enter second number:30
+>>> add=a+b
+>>> sub=a-b
+>>> mul=a/b
+>>> div=a/b
+>>> mul=a*b
+>>> mod=a%b
+>>> fd=a//b
+>>> ex=a**b
+>>> print(add)
+50
+>>> print(sub)
+-10
+>>> print(mul)
+600
+>>> print(div)
+0.6666666666666666
+>>> print(mod)
+20
+>>> print(fd)
+0
+>>> print(ex)
+1073741824000000000000000000000000000000
+>>> 
