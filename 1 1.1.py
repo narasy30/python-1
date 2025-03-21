724333bda8e2e89a77c710753bec35b8451e92e9
narasy30 <reddyakulavenkata@gmail.com>
2025-03-21 14:43:00 +0530

    Add files via upload

---
+++ 1 1.1.py
@@ -0,0 +1,26 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 07:55:33) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=25
+>>> b=35
+>>> add=a+b
+>>> sub=a-b
+>>> mul=a*b
+>>> div=a/b
+>>> mod=a%b
+>>> fd=a//b
+>>> ex=a**b
+>>> print(add)
+60
+>>> print(sub)
+-10
+>>> print(mul)
+875
+>>> print(div)
+0.7142857142857143
+>>> print(mod)
+25
+>>> print(fd)
+0
+>>> print(ex)
+8470329472543003390683225006796419620513916015625
+>>> 
